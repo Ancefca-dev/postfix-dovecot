a4f027e9434066e9aa732dfe512efb1e80db813c
Moov Space <moovspace@gmail.com>
2021-03-26 12:29:11 +0100

    Postfix with mysql
    Postfix dovecot mysql smtp server. Virtual aliases in mysql database.

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# postfix-dovecot
-Postfix dovecot smtp server with mysql server
+# Postfix dovecot
+Postfix dovecot mail server:
+
+
+# Postfix
+
+### Postfix, dovecot install
+```
+sudo apt install postfix dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd mysql-server
+sudo apt install dovecot-mysql postfix-mysql
+```
+
+### User
+```
+groupadd -g 5000 vmail
+useradd -g vmail -u 5000 vmail -d /var/mail
+
+# mail folder !!!
+chown -R root:vmail /var/mail
+```
+
+# Mysql server
+
+### Mysql root user
+```
+# root user password
+sudo mysqladmin -uroot password "toor"
+
+# or
+sudo mysql
+
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('toor');
+SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('toor');
+FLUSH PRIVILEGES;
+```
+
+### Mysql mailuser and db
+```
+sudo mysqladmin -p create mailserver
+
+sudo mysql -p mailserver
+
+GRANT SELECT ON mailserver.* TO 'mailuser'@'127.0.0.1' IDENTIFIED BY 'mailuserpass';
+FLUSH PRIVILEGES;
+```
+
+### Mysql import
+```
+sudo mysql -uroot -p < sql/mailserver.sql
+
+sudo service mysql restart
+```
+
+# Dovecot settings see
+https://www.siemaszko.info/serwer-email-z-postfix-dovecot-na-bazie-mysql-debian-lub-ubuntu/
+
+### Dovectot permissions
+```
+# dovecot
+chown -R vmail:dovecot /etc/dovecot
+chmod -R o-rwx /etc/dovecot
+```
+
+# Errors
+
+### If errors install
+```
+sudo apt install dovecot-mysql postfix-mysql
+```
+
+### Postfix dynamic maps configuration file debian 10 (error was here)
+/etc/postfix/dynamicmaps.cf
+```
+tcp /usr/lib/postfix/dict_tcp.so            dict_tcp_open
+mysql   /usr/lib/postfix/postfix-mysql.so   dict_mysql_open 
+#mysql  /usr/lib/postfix/dict_mysql.so      dict_mysql_open 
+# sqlite  /usr/lib/postfix/dict_sqlite.so   dict_sqlite_open    
+```
+
+### Email dir
+```
+# mail folder !!!
+chown -R root:vmail /var/mail
+```
+
+### Logs
+```
+cat /var/log/mail.log
+
+tail -f /var/log/mail.log
+```
+
+### restart
+```
+sudo service postfix restart
+sudo service dovecot restart
+sudo service mysql restart
+```
+
+### References
+https://www.siemaszko.info/serwer-email-z-postfix-dovecot-na-bazie-mysql-debian-lub-ubuntu/
+https://serverfault.com/questions/861050/postfix-unsupported-dictionary-type-mysql
---
+++ sql/mailserver.sql
@@ -0,0 +1,108 @@
+-- MySQL dump 10.18  Distrib 10.3.27-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mailserver
+-- ------------------------------------------------------
+-- Server version	10.3.27-MariaDB-0+deb10u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `virtual_aliases`
+--
+
+DROP TABLE IF EXISTS `virtual_aliases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_aliases` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) NOT NULL,
+  `source` varchar(100) NOT NULL,
+  `destination` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `domain_id` (`domain_id`),
+  CONSTRAINT `virtual_aliases_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_aliases`
+--
+
+LOCK TABLES `virtual_aliases` WRITE;
+/*!40000 ALTER TABLE `virtual_aliases` DISABLE KEYS */;
+INSERT INTO `virtual_aliases` VALUES (1,1,'alias@example.com','email1@example.com');
+/*!40000 ALTER TABLE `virtual_aliases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_domains`
+--
+
+DROP TABLE IF EXISTS `virtual_domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_domains` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_domains`
+--
+
+LOCK TABLES `virtual_domains` WRITE;
+/*!40000 ALTER TABLE `virtual_domains` DISABLE KEYS */;
+INSERT INTO `virtual_domains` VALUES (1,'example.com'),(2,'hostname.example.com'),(3,'hostname'),(4,'localhost.example.com');
+/*!40000 ALTER TABLE `virtual_domains` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_users`
+--
+
+DROP TABLE IF EXISTS `virtual_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virtual_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) NOT NULL,
+  `password` varchar(106) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `domain_id` (`domain_id`),
+  CONSTRAINT `virtual_users_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_users`
+--
+
+LOCK TABLES `virtual_users` WRITE;
+/*!40000 ALTER TABLE `virtual_users` DISABLE KEYS */;
+INSERT INTO `virtual_users` VALUES (1,1,'$6$2e49b99b554afbc7$GjUNxUBKX2lVlKvUIuAA.0fatLb2s.W4QGiY2VkTpJtGY7MB.B.tPJteDH296A8iKpXRkq6LHtoB2BTBofjXE.','email1@example.com'),(2,1,'$6$125f250cb68b8af2$/Hj1W2BrbUQqPk/YG5LR53shQ2icScjcfo78/d4q2WsrOCQ5IXe.Bk8IlT/6Bwm7WIjamp/2WGPhpZiIOoGj/1','email2@example.com');
+/*!40000 ALTER TABLE `virtual_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-26 11:23:49
